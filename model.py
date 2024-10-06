bf483d36981645d650d9507f02706f198ce2cd77
Rafael Assunção <rafaaslat@gmail.com>
2024-10-06 14:12:45 -0300

    feat: scraper atualizado

--- model.py
+++ model.py
@@ -1,14 +1,22 @@
 class Game:
-    def __init__(self, id, homeTeam, awayTeam, scoreHome, scoreAway):
+    def __init__(self, id, homeTeam, awayTeam, scoreHome, scoreAway, referee, stadium, capacity, public, date, time, country, round):
         self.id = id
         self.homeTeam = homeTeam
         self.awayTeam = awayTeam
-        self.scoreHome = scoreHome
-        self.scoreAway = scoreAway
+        self.scoreHome = int(scoreHome)
+        self.scoreAway = int(scoreAway)
+        self.referee = referee
+        self.stadium = stadium
+        self.capacity = capacity.replace(' ','')
+        self.public = public.replace(' ','')
+        self.date = date
+        self.time = time
+        self.country = country
+        self.round = round
         self.pointsHome, self.pointsAway = self.getPoints(int(scoreHome), int(scoreAway))
     
     def __str__(self):
-        return f"Jogo {self.id}: {self.homeTeam} {self.scoreHome} x {self.scoreAway} {self.awayTeam} | {self.pointsHome}#{self.pointsAway}"
+        return f'Jogo {self.id}: {self.homeTeam} {self.scoreHome} x {self.scoreAway} {self.awayTeam}\n{self.stadium} - {self.public} presentes\n{self.date} | {self.time}\n{self.round}'
     
     def getPoints(self, scoreHome, scoreAway):
         if scoreHome > scoreAway:
--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import time
+import pandas as pd
+from tqdm import tqdm 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,6 +11,17 @@ from model import Game, Team
 def getUrl(gameId): 
     return f'https://www.flashscore.com.br/jogo/{gameId}/#/resumo-de-jogo/resumo-de-jogo'
 
+def getSummaryIndex(text):
+    if text == "ÁRBITRO:":
+        return 0
+    elif text == "ESTÁDIO:":
+        return 1
+    elif text == "CAPACIDADE:":
+        return 2
+    elif text == "PÚBLICO:":
+        return 3
+    else: 
+        return 0
 def getGameInfo(gameId):
     url = getUrl(gameId)
     driver.get(url)
@@ -20,18 +33,46 @@ def getGameInfo(gameId):
                         imageUrl=images[0].get_attribute('src'))
         awayTeam = Team(name=names[1].text,
                         imageUrl=images[1].get_attribute('src'))
+        
+        summary = driver.find_elements(By.CLASS_NAME, '_summaryMatchInformation_1ky8z_4')
+        infoArray = ["","","",""]
+        if len(summary) > 0:
+            summaryLeft = summary[0].find_elements(By.CLASS_NAME, '_infoLabelWrapper_1ky8z_17')
+            summaryRight = summary[0].find_elements(By.CLASS_NAME, '_infoValue_1ky8z_26')
+            for i in range(len(summaryRight)):
+                values = summaryRight[i].find_elements(By.XPATH, './*')
+                leftValue = summaryLeft[i].find_element(By.CLASS_NAME, '_overline_9pgur_4')
+                info = ""
+                for value in values:
+                    info += value.text
+                infoArray[getSummaryIndex(leftValue.text)] = info
+        startTime = driver.find_element(By.CLASS_NAME, 'duelParticipant__startTime').find_element(By.TAG_NAME, 'div').text.split(' ')
+        date = startTime[0].replace('.', '/')
+        time = startTime[1]
+
+        countryHeader = driver.find_element(By.CLASS_NAME, 'tournamentHeader__country')
+        country = countryHeader.text.split(':')[0]
+        round = countryHeader.find_element(By.TAG_NAME, 'a').text
         game = Game(id=gameId,
-                    homeTeam= homeTeam,
-                    awayTeam= awayTeam,
-                    scoreHome= score[0].text,
-                    scoreAway= score[1].text)
+                    homeTeam=homeTeam,
+                    awayTeam=awayTeam,
+                    scoreHome=score[0].text,
+                    scoreAway=score[1].text,
+                    referee=infoArray[0],
+                    stadium=infoArray[1],
+                    capacity=infoArray[2],
+                    public=infoArray[3],
+                    date=date,
+                    time=time,
+                    country=country, 
+                    round=round)
         return game
-    except:
-        print(f'Erro no jogo {gameId}')
+    except Exception as e:
+        print(f'Erro no jogo {gameId}\n{e}')
         return None
 
-def getGamesId():
-    url = 'https://www.flashscore.com.br/futebol/brasil/brasileirao-betano-2022/resultados/'
+def getGamesId(ano):
+    url = f'https://www.flashscore.com.br/futebol/america-do-sul/copa-libertadores-{ano}/resultados/'
     driver.get(url)
     try: 
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler')))
@@ -48,7 +89,6 @@ def getGamesId():
         time.sleep(2)
     gamesId = []
     games = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'eventRowLink')))
-    print(len(games))
     for game in games:
         id = game.get_attribute('href').split('/')[4]
         gamesId.append(id)
@@ -61,11 +101,43 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(service=service, options=options)
-game = getGameInfo('tQx0SNrj')
-print(game)
-#ids = getGamesId()
+array = [2021, 2022, 2023]
+for ano in array:
+    ids = getGamesId(ano)
+    games = []
 
-#for id in ids:
-#    print(getGameInfo(id))
+    for id in tqdm(ids, desc="Buscando jogos..", unit="jogo"):
+        game = getGameInfo(id)
+        if game:
+            games.append(game)
+
+    def getTable(games):
+        data = []
+        for game in games:
+            data.append({
+                'id': game.id,
+                'homeTeam': game.homeTeam.name,
+                'homeTeamImage': game.homeTeam.imageUrl,
+                'awayTeam': game.awayTeam.name,
+                'awayTeamImage': game.awayTeam.imageUrl,
+                'scoreHome': game.scoreHome,
+                'scoreAway': game.scoreAway,
+                'pointsHome': game.pointsHome,
+                'pointsAway': game.pointsAway,
+                'referee': game.referee,
+                'stadium': game.stadium,
+                'capacity': game.capacity,
+                'public': game.public,
+                'date': game.date,
+                'time': game.time,
+                'country': game.country,
+                'round': game.round
+            })
+
+        df_jogos = pd.DataFrame(data)
+
+        df_jogos.to_excel(f'sulamericana_{ano}.xlsx', index=False)
+
+    getTable(games)
 
 driver.quit()
